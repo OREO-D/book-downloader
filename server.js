406bb3c1d5b9f26fd0559bd122bed28be54f130d
0r3on <75869394+OREO-D@users.noreply.github.com>
2021-05-03 19:11:44 -0300

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const PORT = process.env.PORT || 8080
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/static', express.static(path.join(__dirname, 'public')))
-app.get('/', (req, res) => {})
-app.listen(PORT, () => {console.log(`Server is Running...`)})
